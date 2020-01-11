5cb751b614ea2bf06dcc4de0b28eca87690f4ac3
Angie Kirac <angiechiraz@gmail.com>
2020-01-11 16:20:26 -0500

    out of bounds error debug

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,5 +1,5 @@
 import React from "react";
-import elevator from "../../assets/elevator.png";
+import elevatorImg from "../../assets/elevator.png";
 import "../../App.css";
 import { store } from "../../redux/app-redux";
 const unirand = require("unirand");
@@ -11,13 +11,10 @@ unirand.seed(store.getState().seed); // keeping the seed consistent
  direction = -1 means it is heading to answer a call where passengers are trying to go down.
  positions are 0-99, 0 is Lobby
  passengers = # of passengers elevator is CURRENTLY holding, not number of passengers it will be holding after it fulfills it's next call(s) */
-var shaftA = [{ direction: 0 }, { position: 0 }, { passengers: 0 }];
-var shaftB = [{ direction: 0 }, { position: 0 }, { passengers: 0 }];
-var shaftC = [{ direction: 0 }, { position: 0 }, { passengers: 0 }];
 var elevators = [
-  { direction: 0, position: 0, passengers: 0 },
-  { direction: 0, position: 0, passengers: 0 },
-  { direction: 0, position: 0, passengers: 0 }
+  { direction: 0, position: 0, passengers: 0 }, // shaft A
+  { direction: 0, position: 0, passengers: 0 }, // shaft B
+  { direction: 0, position: 0, passengers: 0 } // shaft C
 ];
 
 // create function to take calls as input and output elevator actions
@@ -29,20 +26,30 @@ function makeElevatorActions(calls) {
 
   for (var index = 0; index < calls.length; index++) {
     let call = calls[index];
-    let callDir = getCallDirction(call.origin, call.destination);
-    let elevatorNum = null;
-    if (store.getState().approach === "random")
-      elevatorNum = chooseRandomElevator();
-    else elevatorNum = chooseElevator(call.origin, callDir);
-    actions.push({
-      time: call.time,
-      shaft: elevator,
-      destination: call.destination
-    });
-    console.log(elevatorNum + " in direction " + callDir);
-    // update the chosen elevator
-    updateElevator(elevatorNum, callDir, call.passengers);
+    if (
+      call.origin < 0 ||
+      call.origin > 99 ||
+      call.destination < 0 ||
+      call.destination > 99
+    )
+      console.log("call floor origin or destination out of bounds");
+    else {
+      let callDir = getCallDirction(call.origin, call.destination);
+      let elevatorNum = null;
+      if (store.getState().approach === "random") {
+        elevatorNum = chooseRandomElevator();
+      } else elevatorNum = chooseElevator(call.origin, callDir);
+      // update elevator action time series
+      actions.push({
+        time: call.time,
+        shaft: elevatorNum,
+        destination: call.destination
+      });
+      // update the respective elevator object
+      updateElevator(elevatorNum, callDir, call.passengers);
+    }
   }
+  console.log(actions);
 }
 
 function getCallDirction(origin, destination) {
@@ -52,9 +59,13 @@ function getCallDirction(origin, destination) {
 }
 
 function updateElevator(elevatorNum, callDir, passengers) {
-  let chosenElevator = elevators[elevatorNum - 1];
-  chosenElevator.direction = callDir;
-  console.log(elevators);
+  if (elevatorNum == null) console.log("null elevator");
+  else if (elevatorNum < 1 || elevatorNum > 3)
+    console.log("elevator num out of bounds");
+  else {
+    let chosenElevator = elevators[elevatorNum - 1];
+    chosenElevator.direction = callDir;
+  }
 
   // let currentNumPassengers = chosenElevator.passengers;
   // // if number of passengers will exceed
@@ -65,44 +76,47 @@ function updateElevator(elevatorNum, callDir, passengers) {
 
 // function to choose which elevator responds to the call
 function chooseElevator(callOrigin, callDir) {
-  let posAequalsPosB = shaftA.position === shaftB.position;
-  let posAequalsPosC = shaftA.position === shaftC.position;
-  let posBequalsPosC = shaftB.position === shaftC.position;
+  let posAequalsPosB = elevators[0].position === elevators[1].position;
+  let posAequalsPosC = elevators[0].position === elevators[2].position;
+  let posBequalsPosC = elevators[1].position === elevators[2].position;
 
-  let dirAequalsdirB = shaftA.direction === shaftB.direction;
-  let dirAequalsdirC = shaftA.direction === shaftC.direction;
-  let dirBequalsdirC = shaftB.direction === shaftC.direction;
+  let dirAequalsdirB = elevators[0].direction === elevators[1].direction;
+  let dirAequalsdirC = elevators[0].direction === elevators[2].direction;
+  let dirBequalsdirC = elevators[1].direction === elevators[2].direction;
+  console.log(dirAequalsdirC);
 
-  let distanceA = shaftA.position - callOrigin;
-  let distanceB = shaftB.position - callOrigin;
-  let distanceC = shaftC.position - callOrigin;
+  let distanceA = elevators[0].position - callOrigin;
+  let distanceB = elevators[1].position - callOrigin;
+  let distanceC = elevators[2].position - callOrigin;
+  console.log(distanceC);
 
   // check if shaft is going in the opposite direction of the call
-  let isAmovingOpposite = shaftA.direction === -1 * callDir;
-  let isBmovingOpposite = shaftB.direction === -1 * callDir;
-  let isCmovingOpposite = shaftC.direction === -1 * callDir;
+  let isAmovingOpposite = elevators[0].direction === -1 * callDir;
+  let isBmovingOpposite = elevators[1].direction === -1 * callDir;
+  let isCmovingOpposite = elevators[2].direction === -1 * callDir;
 
   // invalid floor input
-  if (callOrigin < 0 || callOrigin > 99) return -1;
-  else if (posAequalsPosB && posBequalsPosC) {
+  if (callOrigin < 0 || callOrigin > 99) {
+    console.log("call floor out of bounds");
+  } else if (posAequalsPosB && posBequalsPosC) {
     // if all the elevators are equidistant from the call origin
     if (dirAequalsdirB && dirBequalsdirC) return chooseRandomElevator();
     else if (dirAequalsdirB) {
       /* NOTE: I chose to prioritize an elevator not already moving over an elevator moving in the same direction as the call
             - they will not have any passengers and the distance is all the same here */
-      if (shaftA.direction === 0) return chooseBetween(1, 2);
-      else if (shaftC.direction === 0) return 3;
-      else if (shaftA.direction === callDir) return chooseBetween(1, 2);
+      if (elevators[0].direction === 0) return chooseBetween(1, 2);
+      else if (elevators[2].direction === 0) return 3;
+      else if (elevators[0].direction === callDir) return chooseBetween(1, 2);
       else return 3;
     } else if (dirAequalsdirC) {
-      if (shaftA.direction === 0) return chooseBetween(1, 3);
-      else if (shaftB.direction === 0) return 2;
-      else if (shaftA.direction === callDir) return chooseBetween(1, 3);
+      if (elevators[0].direction === 0) return chooseBetween(1, 3);
+      else if (elevators[1].direction === 0) return 2;
+      else if (elevators[0].direction === callDir) return chooseBetween(1, 3);
       else return 2;
     } else if (dirBequalsdirC) {
-      if (shaftB.direction === 0) return chooseBetween(2, 3);
-      else if (shaftA.direction === 0) return 1;
-      else if (shaftB.direction === callDir) return chooseBetween(2, 3);
+      if (elevators[1].direction === 0) return chooseBetween(2, 3);
+      else if (elevators[0].direction === 0) return 1;
+      else if (elevators[1].direction === callDir) return chooseBetween(2, 3);
       else return 1;
     }
     // if elevators A and B are equidistant from the call origin
@@ -204,9 +218,9 @@ function compareAB(dirAequalsdirB, isAmovingOpposite, isBmovingOpposite) {
   if (!isAmovingOpposite) {
     // if neither are moving in the opposite direction of the call, choose either
     if (dirAequalsdirB) return chooseBetween(1, 2);
-    else if (shaftA.direction === 0) return 1;
+    else if (elevators[0].direction === 0) return 1;
     // A has no passengers
-    else if (shaftB.direction === 0) return 2;
+    else if (elevators[1].direction === 0) return 2;
     // B has no passengers
     else return 1; // B is in the opposite direction, return A
   } else if (!isBmovingOpposite) {
@@ -218,8 +232,8 @@ function compareAB(dirAequalsdirB, isAmovingOpposite, isBmovingOpposite) {
 function compareBC(dirBequalsdirC, isBmovingOpposite, isCmovingOpposite) {
   if (!isBmovingOpposite) {
     if (dirBequalsdirC) return chooseBetween(2, 3);
-    else if (shaftB.direction === 0) return 2;
-    else if (shaftC.direction === 0) return 3;
+    else if (elevators[1].direction === 0) return 2;
+    else if (elevators[2].direction === 0) return 3;
     else return 2; // C is moving in wrong direction, return B
   } else if (!isCmovingOpposite) {
     return 3;
@@ -229,8 +243,8 @@ function compareBC(dirBequalsdirC, isBmovingOpposite, isCmovingOpposite) {
 function compareAC(dirAequalsdirC, isAmovingOpposite, isCmovingOpposite) {
   if (!isAmovingOpposite) {
     if (dirAequalsdirC) return chooseBetween(1, 3);
-    else if (shaftA.direction === 0) return 1;
-    else if (shaftC.direction === 0) return 3;
+    else if (elevators[0].direction === 0) return 1;
+    else if (elevators[2].direction === 0) return 3;
     else return 1;
   } else if (!isCmovingOpposite) {
     return 3;
@@ -238,10 +252,14 @@ function compareAC(dirAequalsdirC, isAmovingOpposite, isCmovingOpposite) {
 }
 
 function Loading() {
-  makeElevatorActions([{ time: 0, origin: 6, destination: 10, passengers: 8 }]);
+  makeElevatorActions([
+    { time: 0, origin: 6, destination: 10, passengers: 8 },
+    { time: 0, origin: 11, destination: 6, passengers: 3 },
+    { time: 2, origin: 100, destination: 10, passengers: 8 }
+  ]);
   return (
     <div>
-      <img src={elevator} className="elevator-moving" alt="logo" />
+      <img src={elevatorImg} className="elevator-moving" alt="logo" />
       <p className="loading">
         <code>
           {store.getState().approach === "random"
